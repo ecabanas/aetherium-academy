63c1ff5fab60e5451b245ad6310f8f13bbad5ba4
Eric Cabañas <ecalabcn@gmail.com>
2025-07-06 01:16:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,9 +1,7 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  });
+  admin.initializeApp();
 }
 
 export const firestore = admin.firestore();
