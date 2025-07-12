1a021b2b1e56a1d111aaff4945e05f9e86ed9c2f
Eric Cabañas <ecalabcn@gmail.com>
2025-07-12 18:04:51 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <TableCell>
    
    with

--- src/app/(app)/history/page.tsx
+++ src/app/(app)/history/page.tsx
@@ -130,15 +130,13 @@ export default function HistoryPage() {
                       </Badge>
                     </TableCell>
                     <TableCell>
-                       <div className="max-w-xs overflow-x-auto [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-                        <div className="flex gap-1">
+                       <div className="flex flex-wrap gap-1">
                           {session.summary.split(',').map((keyword, index) => (
                             <Badge key={index} variant="outline" className="font-normal whitespace-nowrap">
                               {keyword.trim()}
                             </Badge>
                           ))}
                         </div>
-                      </div>
                     </TableCell>
                     <TableCell>{formatDistanceToNow(new Date(session.createdAt), { addSuffix: true })}</TableCell>
                     <TableCell className="text-center font-medium">{session.flashcardCount}</TableCell>
